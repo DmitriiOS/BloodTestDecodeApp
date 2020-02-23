fa01e21801d71b3c47b55fa1a2cb2eba2ecbbba9
DmitriiOS <dsorlov86@gmail.com>
2020-02-23 14:50:44 +0300

    Initial Commit

---
+++ BloodTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BloodTest
+//
+//  Created by 1 on 23.02.2020.
+//  Copyright © 2020 DmitriOS. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
