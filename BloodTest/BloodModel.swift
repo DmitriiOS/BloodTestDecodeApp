5de17eda6ec6212ce62895f68cac66c92b6b4dcc
DmitriiOS <dsorlov86@gmail.com>
2020-03-01 22:04:53 +0300

    first commit

---
+++ BloodTest/BloodModel.swift
@@ -0,0 +1,9 @@
+//
+//  BloodModel.swift
+//  BloodTest
+//
+//  Created by 1 on 26.02.2020.
+//  Copyright © 2020 DmitriOS. All rights reserved.
+//
+
+import Foundation
---
+++ BloodTest/CustomTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  CustomTableViewCell.swift
+//  BloodTest
+//
+//  Created by 1 on 26.02.2020.
+//  Copyright © 2020 DmitriOS. All rights reserved.
+//
+
+import UIKit
+
+class CustomTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
